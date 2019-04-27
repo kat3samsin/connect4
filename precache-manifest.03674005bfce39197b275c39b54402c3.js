720fdd642dfdc0eb6ee9d8a6e872b1c3a00a941f
katre <kat3samsin@gmail.com>
2019-04-27 14:54:53 +0800

    Updates

---
+++ precache-manifest.03674005bfce39197b275c39b54402c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eddd70680a642281ffb386984c31a82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3f828450ff1cad6c81",
+    "url": "/static/css/main.797cdd23.chunk.css"
+  },
+  {
+    "revision": "c7b8a49e30453d2b4907",
+    "url": "/static/js/2.79837f49.chunk.js"
+  },
+  {
+    "revision": "fe3f828450ff1cad6c81",
+    "url": "/static/js/main.036b04bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
