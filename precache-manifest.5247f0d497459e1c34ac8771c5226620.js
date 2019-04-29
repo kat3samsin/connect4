d8b2b7ef3d64ea084398403121badb7b301b48fd
katre <kat3samsin@gmail.com>
2019-04-30 06:54:27 +0800

    Updates

--- precache-manifest.5247f0d497459e1c34ac8771c5226620.js
+++ precache-manifest.5247f0d497459e1c34ac8771c5226620.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a92a210dae05e193891e82c64ca83159",
+    "revision": "4ae995c66af24746f9b3c228140d0d65",
     "url": "/connect4/index.html"
   },
   {
-    "revision": "26c8b9c611935693b9d6",
-    "url": "/connect4/static/css/main.a631ed00.chunk.css"
+    "revision": "8a60188588fa81c27de7",
+    "url": "/connect4/static/css/main.6feada5a.chunk.css"
   },
   {
     "revision": "c7b8a49e30453d2b4907",
     "url": "/connect4/static/js/2.79837f49.chunk.js"
   },
   {
-    "revision": "26c8b9c611935693b9d6",
-    "url": "/connect4/static/js/main.9ca57868.chunk.js"
+    "revision": "8a60188588fa81c27de7",
+    "url": "/connect4/static/js/main.a2db89a1.chunk.js"
   },
   {
     "revision": "de9821dd79bb17df15fc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/connect4/precache-manifest.2d7708cb191ce07d9e9749c9db4327ed.js"
+  "/connect4/precache-manifest.5247f0d497459e1c34ac8771c5226620.js"
 );
 
 self.addEventListener('message', (event) => {
